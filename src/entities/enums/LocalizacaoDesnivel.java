5bd7f7761246c3d69aad9cea92b7495edafb4bc4
Júlio César <52138888+julnox@users.noreply.github.com>
2021-04-09 13:24:04 -0300

    Initial Project Enums

---
+++ src/entities/enums/LocalizacaoDesnivel.java
@@ -0,0 +1,9 @@
+package entities.enums;
+
+public enum LocalizacaoDesnivel {
+    RUA (0),
+    CALCADA (1);
+
+    LocalizacaoDesnivel(Integer n){
+    }
+}
---
+++ src/entities/enums/Situacao.java
@@ -0,0 +1,9 @@
+package entities.enums;
+
+public enum Situacao {
+    REGISTRADO (0),
+    RESOLVIDO (1);
+
+    Situacao(Integer n) {
+    }
+}
